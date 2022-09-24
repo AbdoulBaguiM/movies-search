a64f2a381dd90c3c77b784fbb1f552a1deb74522
Abdelhk <mhdabdel151@gmail.com>
2022-09-24 22:56:36 +0100

    retrieve and index data

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const client = require("./elasticsearch/client");
+const moviesRouter = require("./data/movie_api");
 
 const app = express();
+
+app.use("/movies", moviesRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
